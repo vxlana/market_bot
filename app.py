d091567e03387170d8dcaa0968b2133ea16ac49f
vxlana <r4kode@yahoo.com>
2025-09-03 12:24:00 +0000

    first commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
-bot = Bot(token="8245378307:AAHxCB6cnl37OEt2Wm8rdPRB3vBfMbOGXsE")
+bot = Bot(token="#")
 
 dp = Dispatcher()
 
@@ -13,15 +13,8 @@ async def start_cmd(message: types.Message):
 
 
 @dp.message()
-async def echo(message: types.Message) -> None:
-    text: str | None = message.text
-
-    if text in ['Привет', 'привет', 'hi', 'hello']:
-        await message.answer('И тебе привет!')
-    elif text in ['Пока', 'пока', 'пакеда', 'До свидания']:
-        await message.answer('И тебе пока')
-    else:
-        await message.answer(message.text)
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 
 async def main():
