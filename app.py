b66c30487c4a78b80725e0273da8705701cfdeb9
vxlana <r4kode@yahoo.com>
2025-09-03 20:39:06 +0000

    router

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
 import asyncio
+import os
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
 from dotenv import find_dotenv, load_dotenv
-import os
-
 load_dotenv(find_dotenv)
+
+from handlers.user_private import user_private_router
+
+ALLOWED_UPDATES = ['message', 'edited_message']
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-        await message.answer('Это была команда СТАРТ')
-
-
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)       
-    await message.reply(message.text)  
+dp.include_router(user_private_router)
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 asyncio.run(main())
