4604c0b29596f0808c582b7ed53a18933fcff253
vxlana <r4kode@yahoo.com>
2025-09-05 17:23:33 +0000

    end user_private

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,5 +1,5 @@
-from aiogram import types, Router
-from aiogram.filters import CommandStart, Command
+from aiogram import F, types, Router
+from aiogram.filters import CommandStart, Command, or_f
 
 user_private_router = Router()
 
@@ -8,21 +8,30 @@ user_private_router = Router()
 async def start_cmd(message: types.Message):
         await message.answer('Привет, я виртуальный помощник')
 
-
-@user_private_router.message(Command('menu'))
+# @user_private_router.message(F.text.lower() == 'меню')
+@user_private_router.message(or_f(Command('menu'), (F.text.lower() == 'menu')))
 async def menu_cmd(message: types.Message):
     await message.answer('Вот меню:')       
 
+@user_private_router.message(F.text.lower() == 'о нас')
 @user_private_router.message(Command('about'))
 async def about_cmd(message: types.Message):
       await message.answer('О нас:')
 
+
+@user_private_router.message(F.text.lower() == 'варианты оплаты')
 @user_private_router.message(Command('payment'))
 async def payment_cmd(message: types.Message):
       await message.answer('Варианты оплаты')
 
+
+@user_private_router.message((F.text.lower().contains('доставк')) | (F.text.lower() == 'варианты доставки'))
 @user_private_router.message(Command('shipping'))
 async def shipping_cmd(message: types.Message):
       await message.answer('Варианты доставки')
+
+
+
+
 
 
