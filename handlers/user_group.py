c147b6a7f015bbadd5899d6d66e32b8ca0470492
vxlana <r4kode@yahoo.com>
2025-09-08 14:23:00 +0000

    filters

--- handlers/user_group.py
+++ handlers/user_group.py
@@ -1,14 +1,17 @@
 from string import punctuation
 
 from aiogram import F, types, Router 
+from filters.chat_types import ChatTypeFilter
 
 user_group_router = Router()
+user_group_router.message.filter(ChatTypeFilter(['group', 'supergroup']))
 
 restricted_words = {'кабан', 'мат'}
 
 def clean_text(text: str):
     return text.translate(str.maketrans('', '', punctuation))
 
+@user_group_router.edited_message()
 @user_group_router.message()
 async def cleaner(message: types.Message):
     if restricted_words.intersection(clean_text(message.text.lower()).split()):
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,7 +1,9 @@
 from aiogram import F, types, Router
 from aiogram.filters import CommandStart, Command, or_f
+from filters.chat_types import ChatTypeFilter
 
 user_private_router = Router()
+user_private_router.message.filter(ChatTypeFilter(['private']))
 
 
 @user_private_router.message(CommandStart())
