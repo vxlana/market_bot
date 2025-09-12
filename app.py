fadad927813e8b572420b3989b9d932a70d6a3d7
vxlana <r4kode@yahoo.com>
2025-09-12 09:23:24 +0000

    correct

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import asyncio
 import os
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-
-from aiogram.types import BotCommandScopeAllPrivateChats
-from common.bot_commands_list import private
 
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
@@ -16,9 +12,13 @@ from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
 
-ALLOWED_UPDATES = ['message', 'edited_message']
+from common.bot_commands_list import private
 
-bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+ALLOWED_UPDATES = ['message, edited_message']
+
+bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML) )
+bot.my_admins_list = []
+
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
@@ -29,7 +29,7 @@ dp.include_router(admin_router)
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
-    await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 asyncio.run(main())
